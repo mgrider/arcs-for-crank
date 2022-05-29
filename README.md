ba51952fcca384fddb5a4d10a291b058ec6c589c
Martin Grider <mgrider@gmail.com>
2022-05-29 15:27:53 -0500

    first version.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Arcs for Crank
+
+This is more or less a port of the "arcs" example in the PlaydateSDK (single file examples) to use the crank.
+
+As soon as I saw that example, I wanted to make it work with the crank, and after working with the sdk for a week or so, I found it was easy. I did add some bells and whistles. You can change the number of arcs and invert the drawing from the system menu. Toggle auto-draw with A, and reset the drawing with B.
+
---
+++ source/main.lua
@@ -0,0 +1,134 @@
+import 'CoreLibs/graphics'
+
+gfx = playdate.graphics
+
+gfx.clear()
+
+startAngle = 0
+ds = 0.4
+endAngle = 0
+de = 2
+radius = 120
+nrings = 12
+
+innerRadius = 1 * radius/nrings
+outerRadius = 2 * radius/nrings
+
+autodraw = false
+inverted = false
+
+gfx.setStrokeLocation(gfx.kStrokeOutside)
+
+-- set some menu items
+
+local menu = playdate.getSystemMenu()
+local menuItem, error = menu:addMenuItem("Reset", function()
+	reset()
+end)
+local menuItem, error = menu:addMenuItem("Randomize", function()
+	randomize()
+end)
+local menuItem, error = menu:addMenuItem("Invert", function()
+	doInvert()
+end)
+
+-- update
+
+function playdate.update()
+
+	gfx.clear()
+
+	gfx.setColor(gfx.kColorWhite);
+	gfx.drawRect(200 - radius, 120 - radius, radius * 2 + 1, radius * 2 + 1)
+
+	if autodraw then
+		startAngle = startAngle + ds
+		endAngle = endAngle + de
+	end
+
+	gfx.setColor(gfx.kColorBlack);
+
+	for i=1,nrings-1
+	do
+		innerRadius = i * radius/nrings
+		outerRadius = (i+1) * radius/nrings
+		gfx.setLineWidth(outerRadius - innerRadius)
+		gfx.drawArc(200, 120, innerRadius, (nrings - i) * startAngle, (nrings - i) * endAngle)
+	end
+
+end
+
+
+function drawarc()
+	gfx.setColor(gfx.kColorWhite);
+	gfx.drawRect(0, 0, 400, 240)
+
+	gfx.setColor(gfx.kColorXOR)
+	gfx.setLineWidth(outerRadius - innerRadius)
+	gfx.drawArc(200, 120, innerRadius, startAngle, endAngle)
+	playdate.display.flush()
+end
+
+
+function playdate.upButtonDown()
+	nrings += 1
+	drawarc()
+end
+
+function playdate.downButtonDown()
+	nrings -= 1
+	if nrings < 2 then nrings = 2 end
+	drawarc()
+end
+
+function playdate.leftButtonDown()
+	endAngle = endAngle - 10
+	drawarc()
+end
+
+function playdate.rightButtonDown()
+	endAngle = endAngle + 10
+	drawarc()
+end
+
+function playdate.AButtonDown()
+	autodraw = not autodraw
+end
+
+function playdate.BButtonDown()
+	reset()
+end
+
+function playdate.cranked(change, acceleratedChange)
+	startAngle += acceleratedChange / 10
+	endAngle += acceleratedChange / 2
+end
+
+function reset()
+	gfx.clear()
+	startAngle = 0
+	ds = 0.4
+	endAngle = 0
+	de = 2
+	radius = 120
+	nrings = 12
+	innerRadius = 1 * radius/nrings
+	outerRadius = 2 * radius/nrings
+	drawarc()
+end
+
+function randomize()
+	gfx.clear()
+	startAngle = math.random(120)
+	endAngle = math.random(120)
+--	radius = math.random(120)
+	nrings = math.random(2,24)
+	innerRadius = math.random(120)
+	outerRadius = math.random(120)
+	drawarc()
+end
+
+function doInvert()
+	inverted = not inverted
+	playdate.display.setInverted(inverted)
+end
